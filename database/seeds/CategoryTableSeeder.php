ed82aa5361020635e8f0add390d76897151584ea
Your Name <you@example.com>
2017-08-29 16:13:59 +0700

    update seed category

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
-class DatabaseSeeder extends Seeder
+class CategoryTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CategoryTableSeeder::class);
+    	Category::truncate();
+    	
+        factory(Category::class, 2)->create();
     }
 }
